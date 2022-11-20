9c9ea1f41a3bd66e21e6b4fd52ec898b93ef3055
Tuckahoe <2510055954@qq.com>
2022-11-20 13:48:57 +0800

    修改及新建书本信息

--- app/src/main/java/com/example/mybookshelf/Book_Detail.kt
+++ app/src/main/java/com/example/mybookshelf/Book_Detail.kt
@@ -1,8 +1,13 @@
 package com.example.mybookshelf
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class Book_Detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,64 @@ class Book_Detail : AppCompatActivity() {
         val back=findViewById<ImageView>(R.id.back)
         back.setOnClickListener{
             finish()
+        }
+        //接收书本index
+        val bookIndex:Int=intent.getIntExtra("index",0)
+        //获取控件
+        val bookImage=findViewById<ImageView>(R.id.bookImage)
+        val bookTitle=findViewById<TextView>(R.id.bookTitle)
+        val bookAuthor=findViewById<TextView>(R.id.bookAuthor)
+        val bookPubDate=findViewById<TextView>(R.id.bookPubDate)
+        val bookPublisher=findViewById<TextView>(R.id.bookPublisher)
+        val bookISBN=findViewById<TextView>(R.id.bookISBN)
+        val bookCondition=findViewById<TextView>(R.id.bookCondition)
+        val bookPosition=findViewById<TextView>(R.id.bookPosition)
+        val bookNote=findViewById<TextView>(R.id.bookNote)
+        val bookLable=findViewById<TextView>(R.id.bookLable)
+        val bookLink=findViewById<TextView>(R.id.bookLink)
+        if(bookIndex != 0) {
+            //赋值
+            val bookDetail=getSharedPreferences(bookIndex.toString(),Context.MODE_PRIVATE)
+            bookImage.setImageResource(bookDetail.getInt("imageId",-2))
+            bookTitle.text=bookDetail.getString("title","Default Title")
+            bookAuthor.text=bookDetail.getString("author","Default Author")
+            bookPubDate.text=bookDetail.getString("time","0000-0")
+            bookPublisher.text=bookDetail.getString("publisher","Unknown")
+            bookISBN.text=bookDetail.getString("ISBN","00000000")
+            bookCondition.text=bookDetail.getString("condition","Unknown")
+            bookPosition.text=bookDetail.getString("position","Unknown")
+            bookNote.text=bookDetail.getString("note","none")
+            bookLable.text=bookDetail.getString("lable","Unset")
+            bookLink.text=bookDetail.getString("link","Unknown")
+        }
+
+        //save按钮点击事件
+        val save=findViewById<TextView>(R.id.save)
+        save.setOnClickListener {
+            var theBookIndex=0
+            theBookIndex = if(bookIndex != 0) {
+                bookIndex
+            } else {
+                val bookSum=intent.getIntExtra("bookSum",0)
+                val menuEditor=getSharedPreferences("menu", Context.MODE_PRIVATE).edit()
+                menuEditor.putInt("sum",bookSum+1)//总数+1
+                menuEditor.apply()
+                bookSum+1
+            }
+            val bookEditor=getSharedPreferences(theBookIndex.toString(),Context.MODE_PRIVATE).edit()
+            bookEditor.putString("title", bookTitle.text.toString())
+            bookEditor.putString("author",bookAuthor.text.toString())
+            bookEditor.putString("time",bookPubDate.text.toString())
+            bookEditor.putString("publisher",bookPublisher.text.toString())
+            bookEditor.putString("ISBN",bookISBN.text.toString())
+            bookEditor.putString("condition",bookCondition.text.toString())
+            bookEditor.putString("position",bookPosition.text.toString())
+            bookEditor.putString("note",bookNote.text.toString())
+            bookEditor.putString("lable",bookLable.text.toString())
+            bookEditor.putString("link",bookLink.text.toString())
+            //bookEditor.putInt("imageId",bookImage.id)
+            bookEditor.apply()
+            Toast.makeText(this,"data saved",Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/mybookshelf/MainActivity.kt
+++ app/src/main/java/com/example/mybookshelf/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mybookshelf
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.appcompat.widget.Toolbar
 import android.widget.*
@@ -28,14 +30,65 @@ class MainActivity : AppCompatActivity() {
             it.setDisplayHomeAsUpEnabled(true)
             it.setHomeAsUpIndicator(R.drawable.ic_nav_menu)
         }
+
+        //“目录”文件
+        val menuEditor=getSharedPreferences("menu", Context.MODE_PRIVATE).edit()
+        menuEditor.putInt("sum",2)
+        menuEditor.apply()
+        //在内存中添加一些书本信息
+//        var bookEditor=getSharedPreferences("1", Context.MODE_PRIVATE).edit()
+//        bookEditor.putString("title","Kokomi Love Ayaka!")
+//        bookEditor.putInt("imageId",R.drawable.book1)
+//        bookEditor.putString("author","Tuckahoe")
+//        bookEditor.putString("time","2022-11")
+//        bookEditor.putString("publisher","ZhuHai Publisher")
+//        bookEditor.putString("ISBN","20013289")
+//        bookEditor.putString("condition","Reading")
+//        bookEditor.putString("position","Default Bookshelf")
+//        bookEditor.putString("note","Notes")
+//        bookEditor.putString("lable","fiction")
+//        bookEditor.putString("link","http://tuckahoes")
+//        bookEditor.putInt("index",1)
+//        bookEditor.apply()
+//        bookEditor=getSharedPreferences("2", Context.MODE_PRIVATE).edit()
+//        bookEditor.putString("title","决战!Ayaka!")
+//        bookEditor.putInt("imageId",R.drawable.book2)
+//        bookEditor.putString("author","Kokomi")
+//        bookEditor.putString("time","2020-10")
+//        bookEditor.putString("publisher","XiuShan Publisher")
+//        bookEditor.putString("ISBN","20013289")
+//        bookEditor.putString("condition","Unread")
+//        bookEditor.putString("position","Default Bookshelf")
+//        bookEditor.putString("note","Notes")
+//        bookEditor.putString("lable","fiction")
+//        bookEditor.putString("link","http://tuckahoes")
+//        bookEditor.putInt("index",2)
+//        bookEditor.apply()
+
+        //从目录文件中读取书本数量等信息
+        val bookMenu=getSharedPreferences("menu",Context.MODE_PRIVATE)
+        val bookSum=bookMenu.getInt("sum",0)
+
         //初始化书本数据
-        initBook()
+        repeat(5){
+            for(i in 1..bookSum){
+                val bookMessage=getSharedPreferences(i.toString(),Context.MODE_PRIVATE)
+                val bookTitle=bookMessage.getString("title","Default Title")
+                val bookImageId=bookMessage.getInt("imageId",0)
+                val bookAuthor=bookMessage.getString("author","Author")
+                val bookTime=bookMessage.getString("time","0000-0")
+                val bookIndex=bookMessage.getInt("index",-1)
+                bookList.add(Book(bookTitle.toString(),bookImageId,bookAuthor.toString(),bookTime.toString(),bookIndex))
+            }
+        }
         val adapter=BookAdapter(this,R.layout.books_meau,bookList)
         val listView: ListView =findViewById(R.id.listView)
         listView.adapter=adapter
-            //主页面书本点击事件
+        //主页面书本点击事件
         listView.setOnItemClickListener{parent,view,position,id->
             val intent = Intent(this,Book_Detail::class.java)
+            val clickedBook = bookList[position]
+            intent.putExtra("index",clickedBook.index)
             startActivity(intent)
         }
 
@@ -48,20 +101,22 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.closeDrawers()
             true
         }
-        //悬浮按钮点击事件
+        //点击悬浮按钮添加书本
         val fab=findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
-            Toast.makeText(this,"FAB clicked",Toast.LENGTH_SHORT).show()
+            val intent =Intent(this,Book_Detail::class.java)
+            intent.putExtra("index",0)
+            intent.putExtra("bookSum",bookSum)
+            startActivity(intent)
         }
     }
 
-    //加载布局文件
+    //加载抽屉菜单栏布局文件
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar,menu)
         return true
     }
-
-    //加入菜单项点击事件
+    //菜单项点击事件
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val drawerLayout =findViewById<DrawerLayout>(R.id.drawerLayout)
         when (item.itemId) {
@@ -71,24 +126,14 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
-
-
-
-    private fun initBook() {
-        repeat(5) {
-            bookList.add(Book("book1's title",R.drawable.book1,"author1","2020-1"))
-            bookList.add(Book("book2's title",R.drawable.book2,"author2","2020-2"))
-        }
-    }
 }
 
-//书类,属性为书名、图片ID、书的信息（作者等）、出版时间
-class Book(var title:String, var imageId: Int,var message:String, var time:String )
-
+//书类,属性为书名、图片ID、书的信息（作者等）、出版时间、索引值
+class Book(var title:String, var imageId: Int,var author:String, var time:String,val index:Int )
 //适配器
 class BookAdapter(activity:Activity,val resourceId:Int ,data:List<Book>):
         ArrayAdapter<Book>(activity,resourceId,data) {
-    inner class ViewHolder(val bookImage:ImageView,val bookTitle:TextView)
+    inner class ViewHolder(val bookImage:ImageView,val bookTitle:TextView,val bookAuthor:TextView,val bookTime:TextView)
             override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
                 val view:View
                 val viewHolder:ViewHolder
@@ -96,7 +141,9 @@ class BookAdapter(activity:Activity,val resourceId:Int ,data:List<Book>):
                     view = LayoutInflater.from(context).inflate(resourceId,parent,false)
                     val bookImage:ImageView=view.findViewById(R.id.book_image)
                     val bookTitle: TextView=view.findViewById(R.id.book_title)
-                    viewHolder=ViewHolder(bookImage,bookTitle)
+                    val bookAuthor:TextView=view.findViewById(R.id.book_author)
+                    val bookTime:TextView=view.findViewById(R.id.book_time)
+                    viewHolder=ViewHolder(bookImage,bookTitle,bookAuthor,bookTime)
                     view.tag=viewHolder
                 }else{
                     view = convertView
@@ -106,6 +153,8 @@ class BookAdapter(activity:Activity,val resourceId:Int ,data:List<Book>):
                 if(book!=null) {
                     viewHolder.bookImage.setImageResource(book.imageId)
                     viewHolder.bookTitle.text=book.title
+                    viewHolder.bookAuthor.text=book.author
+                    viewHolder.bookTime.text=book.time
                 }
             return view
             }
